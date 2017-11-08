bcfe746d80633daec179f9ce15278fadee980511
Shane <microbistro@gmail.com>
2017-11-08 15:11:55 -0800

    continue adding js to return a total price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 //back-end
 
+function YourPizza(yourSize, yourToppings, yourPrice) {
+ this.size = yourSize;
+ this.toppings = yourToppings;
+ this.price = yourPrice;
+ }
+
+  var pizza = new YourPizza("#size", ["#toppings"])
+
+  YourPizza.prototype.calculatePrice()
+  if yourPizza.yourSize === 'personal ($9)' {
+    yourPizza.price = 9 + yourToppings.length;
+  }
+  else if yourPizza.yourSize === 'medium ($13)' {
+    yourPizza.price = 13 + yourToppings.length;
+  }
+  else if yourPizza.yourSize === 'big bambino! ($18)' {
+    yourPizza.price = 18 + yourToppings.length;
+  };
+
+
+
+
+
+
 
 //front-end
 $(document).ready(function() {
@@ -12,11 +36,11 @@ $(document).ready(function() {
 
     $("input:checkbox[name=pizza-topping]:checked").each(function(){
       var allYourToppings = $(this).val();
-      console.log(allYourToppings);
       $(".toppings").append(allYourToppings + " ");
     });
 
 
     $("#show-your-order").show();
+    $(".total-price").text(YourPizza.yourPrice.toString());
   });
 });
